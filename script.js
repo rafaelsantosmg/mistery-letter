956238953b038d68b4141b031d2f7398058459c0
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-09-14 21:43:36 -0300

    requisito 7

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ const buttonCreate = document.querySelector('#criar-carta');
 
 buttonCreate.addEventListener('click', () => {
   paragraph.innerHTML = '';
-  if (imputLetter.value === '') {
-    imputLetter.value = 'Por Favor, digite o conteúdo da carta';
-  } else {
-    const phrase = imputLetter.value.split(' ');
-    for (let index = 0; index < phrase.length; index += 1) {
-      const createSpan = document.createElement('span');
-      createSpan.innerHTML = phrase[index];
-      paragraph.appendChild(createSpan);
-    }
+  if (imputLetter.value.trim() === '') {
+    paragraph.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  }
+  const phrase = imputLetter.value.split(' ');
+  for (let index = 0; index < phrase.length; index += 1) {
+    const createSpan = document.createElement('span');
+    createSpan.innerHTML = phrase[index];
+    paragraph.appendChild(createSpan);
   }
 });
