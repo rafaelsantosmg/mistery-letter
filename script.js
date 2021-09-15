512325a8121ac43954e0667b60eab107d4084fc4
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-09-14 21:33:07 -0300

    requisito do 4 ao 6

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const buttonCreate = document.querySelector('#criar-carta');
 
 buttonCreate.addEventListener('click', () => {
   paragraph.innerHTML = '';
-  console.log(imputLetter);
-  const phrase = imputLetter.value.split(' ');
-  for (let index = 0; index < phrase.length; index += 1) {
-    const createSpan = document.createElement('span');
-    createSpan.innerHTML = phrase[index];
-    paragraph.appendChild(createSpan);
+  if (imputLetter.value === '') {
+    imputLetter.value = 'Por Favor, digite o conteúdo da carta';
+  } else {
+    const phrase = imputLetter.value.split(' ');
+    for (let index = 0; index < phrase.length; index += 1) {
+      const createSpan = document.createElement('span');
+      createSpan.innerHTML = phrase[index];
+      paragraph.appendChild(createSpan);
+    }
   }
 });
